e25fcfea8f07ccfeecdf181350c5041d2998f00d
prakhar16 <sharmaprakhar1605@gmail.com>
2025-07-29 12:05:06 -0800

    add modal reusable component

---
+++ src/components/modal.jsx
@@ -0,0 +1,34 @@
+import CommonButton from "./button";
+
+export const CommonModal = ({ children, close, modalTitle ,modalFooter}) => {
+  return (
+    <div
+      className="modal show fade d-block"
+      tabIndex="-1"
+      role="dialog"
+      style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+    >
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{modalTitle}</h5>
+            <CommonButton
+              type="button"
+              className="btn-close"
+              onClick={() => close(false)}
+            />
+          </div>
+          <div className="modal-body">{children}</div>
+ 
+
+ {modalFooter &&  <div className = "modal-footer">
+       {modalFooter}
+    </div>}
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
